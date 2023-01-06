58e4da157f3526cf2636d0db6316484402ba2ad6
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2023-01-06 17:50:50 -0500

    Archivos iniciales

---
+++ src/POO/Venta.java
@@ -0,0 +1,20 @@
+package POO;
+
+/**
+ *    Fundamentos de programación orientada a eventos 750014C-01  
+ *    Laboratorio # 
+ *    Profesor: Luis Romo Portilla 
+ *
+ *    Archivo:  Supermercado.java
+ *    Licencia: GNU-GPL 
+ *    @version  1.0
+ *    
+ *    @author   Alejandro Guerrero Cano           (202179652-3743) {@literal <"alejandro.cano@correounivalle.edu.co">}
+ *    @author   Estiven Andres Martinez Granados  (202179687-3743) {@literal <"estiven.martinez@correounivalle.edu.co">}
+ *    @author   Juan David Loaiza Santiago        (202177570-3743) {@literal <"juan.loaiza.santiago@correounivalle.edu.co">}
+ * 
+*/
+
+public class Venta {
+
+}
