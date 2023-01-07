ff6935f100bc4ec595dbd3533e8001b598147635
Juan Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2023-01-07 01:41:28 -0500

    Cambio de nombre del paquete

---
+++ src/main/main.java
@@ -0,0 +1,22 @@
+package main;
+
+/**
+ *    Fundamentos de programación orientada a eventos 750014C-01  
+    Laboratorio # 
+    Profesor: Luis Romo Portilla 
+
+    Archivo:  main.java
+    Licencia: GNU-GPL 
+ *    @version  1.0
+ *    
+ *    @author   Alejandro Guerrero Cano           (202179652-3743) {@literal <"alejandro.cano@correounivalle.edu.co">}
+ *    @author   Estiven Andres Martinez Granados  (202179687-3743) {@literal <"estiven.martinez@correounivalle.edu.co">}
+ *    @author   Juan David Loaiza Santiago        (202177570-3743) {@literal <"juan.loaiza.santiago@correounivalle.edu.co">}
+ * 
+*/
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
